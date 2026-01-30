556daf3d8b237c58e1dad3438709f02713b4d157
almightydeveloper <code.pablo.development@gmail.com>
2026-01-30 20:59:22 +0100

    fix recommendations

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,4 +13,7 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+
+export const db = initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+})
